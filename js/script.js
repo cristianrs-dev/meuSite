9b1e1d268e0a5ee6cc0e6fa1ad5d968f46a3c322
eclipseCJP <cris.tecwordd@gmail.com>
2022-11-14 20:53:13 -0400

    menu mobile ajustado

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 alert("site em construção");
 
 function openMenu(){
-    document.getElementById("myDropdown").classList.toggle("show");
+    //document.getElementById("myDropdown").classList.toggle("show");
+    let menu=document.getElementById("mobile")
+    if(menu.style.display =='block'){
+        menu.style.display='none'
+    }else{
+        menu.style.display='block'
+    }
 }
