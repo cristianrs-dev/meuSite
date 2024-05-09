029b5ef56d27a68c4042c6d4af71e6c074df4902
eclipseCJP <cris.tecwodd@gmail.com>
2024-05-08 21:25:53 -0400

    primeiro projeto desktop

--- js/script.js
+++ js/script.js
@@ -9,4 +9,40 @@ function openMenu(){
         menu.style.display='block'
     }
 }
+//abas
+let abaFront = document.getElementById("front")
+let abaBack = document.getElementById("back")
+let abaTeste = document.getElementById("teste")
 
+//divs
+let frontEnd = document.getElementById("front-end")
+let backEnd = document.getElementById("back-end")
+let testes = document.getElementById("testes")
+backEnd.style.display='none'
+testes.style.display='none'
+
+abaFront.addEventListener('click',function mostrarConteudo(){
+    frontEnd.style.display='block'
+    backEnd.style.display='none'
+    testes.style.display='none'
+})
+
+abaBack.addEventListener('click',function mostrarConteudo(){
+    abaBack.classList.add('active')
+    backEnd.style.display='block'
+    abaFront.classList.remove('active')
+    abaTeste.classList.remove('active')
+
+    frontEnd.style.display='none'
+    testes.style.display='none'
+})
+
+abaTeste.addEventListener('click',function mostrarConteudo(){
+    abaTeste.classList.add('active')
+
+    abaFront.classList.remove('active')
+    abaBack.classList.remove('active')
+
+    frontEnd.style.display='none'
+    backEnd.style.display='none'
+})
